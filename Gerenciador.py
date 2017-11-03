b3a40662198a6496e451a4b294299bfe026295f7
jvrobinho <jvmonteirorj@gmail.com>
2017-11-03 11:48:11 -0200

    Começo do menu de usuario
    Signed-off-by: jvrobinho <jvmonteirorj@gmail.com>

---
+++ Gerenciador.py
@@ -0,0 +1,23 @@
+print("Bem vindo ao programa!!")
+opcao = -1
+while (opcao != 6):
+    print("1. Criar uma nova Wallet\n2. Criar um novo cartão para um usuário")
+    print("3. Vincular cartão a uma Wallet\n4. Remover um cartão da Wallet de um usuário")
+    print("5. Ver informações da Wallet\n6.Realizar uma compra de valor X com uma wallet")
+    print("6. Sair do programa")
+    opcao = eval(input("Digite a opção desejada: "))
+
+    #TODO opções do menu
+    if(opcao == 1):
+       print("do something")
+    elif (opcao == 2):
+        print("do something")
+    elif (opcao == 3):
+        print("do something")
+    elif (opcao == 4):
+        print("do something")
+    elif (opcao == 5):
+        print("do something")
+    elif (opcao == 6):
+        print("Obrigado por usar nosso aplicativo :) Volte sempre!")
+        exit(0)
